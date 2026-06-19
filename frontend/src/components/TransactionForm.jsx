84b45cebcc28563a2b7e39796bc97766cfedbaaa
Piyush Jain <Piyushjain1857@gmail.com>
2026-06-19 11:33:38 +0530

    fixed bugs

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -24,11 +24,8 @@ const TransactionForm = ({ fetchTransactions }) => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     await api.post('/transactions/', formData);
-
     fetchTransactions();
-
     setFormData({
       amount: '',
       category: '',
@@ -41,78 +38,31 @@ const TransactionForm = ({ fetchTransactions }) => {
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="mb-3 mt-3">
-        <label htmlFor="amount" className="form-label">
-          Amount
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="amount"
-          name="amount"
-          value={formData.amount}
-          onChange={handleInputChange}
-        />
+        <label htmlFor="amount" className="form-label">Amount</label>
+        <input type="text" className="form-control" id="amount" name="amount" value={formData.amount} onChange={handleInputChange} />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="category" className="form-label">
-          Category
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="category"
-          name="category"
-          value={formData.category}
-          onChange={handleInputChange}
-        />
+        <label htmlFor="category" className="form-label">Category</label>
+        <input type="text" className="form-control" id="category" name="category" value={formData.category} onChange={handleInputChange} />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          description
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleInputChange}
-        />
+        <label htmlFor="description" className="form-label">Description</label>
+        <input type="text" className="form-control" id="description" name="description" value={formData.description} onChange={handleInputChange} />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="is_income" className="form-label">
-          Income..!?
-        </label>
-        <input
-          type="checkbox"
-          className="form-check-input ms-2"
-          id="is_income"
-          name="is_income"
-          checked={formData.is_income}
-          onChange={handleInputChange}
-        />
+        <label htmlFor="is_income" className="form-label">Income..?</label>
+        <input type="checkbox" className="form-check-input ms-2" id="is_income" name="is_income" checked={formData.is_income} onChange={handleInputChange} />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="date" className="form-label">
-          Date
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="date"
-          name="date"
-          value={formData.date}
-          onChange={handleInputChange}
-        />
+        <label htmlFor="date" className="form-label">Date</label>
+        <input type="text" className="form-control" id="date" name="date" value={formData.date} onChange={handleInputChange} />
       </div>
 
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
+      <button type="submit" className="btn btn-primary"> Submit </button>
     </form>
   );
 };
